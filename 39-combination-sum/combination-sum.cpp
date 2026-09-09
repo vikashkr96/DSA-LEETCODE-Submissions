9d6a5bee3a8b2dfafd750b94aeeb9ff6b62e9a81
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-09-10 00:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (93.42%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
 
-    void solve(vector<int>& candidates, int target, int index,
-               vector<int>& curr, vector<vector<int>>& ans) {
-
+    void findCombo(int idx, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
         // base case
-        if(target == 0) {
-            ans.push_back(curr);
+        if(idx == arr.size()){
+            if(target == 0){
+                ans.push_back(ds);
+            }
             return;
         }
 
-        if(index == candidates.size() || target < 0)
-            return;
+        // picking up 
+        if(arr[idx] <= target){
+            ds.push_back(arr[idx]);
+            findCombo(idx, target-arr[idx], arr, ans, ds);
+            // backtrack
+            ds.pop_back();
+        }
 
-
-        // picking current element 
-        curr.push_back(candidates[index]);
-
-        solve(candidates, target - candidates[index], index, curr, ans);
-
-        // backtrack
-        curr.pop_back();
-
-
-        // skipping current element
-        solve(candidates, target, index + 1, curr, ans);
+        // not picking up
+        findCombo(idx+1, target, arr, ans, ds);
+        
     }
-
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
 
         vector<vector<int>> ans;
-        vector<int> curr;
-
-        solve(candidates, target, 0, curr, ans);
-
+        vector<int> ds;
+        findCombo(0,target, candidates, ans, ds);
         return ans;
     }
 };
